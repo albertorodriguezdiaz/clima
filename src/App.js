405c1eb56eaa102ab655b40d478457333ed955d7
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-10-28 16:25:15 -0500

    Start APP Clima

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header 
+        titulo='Clima React App'
+      />
+        
+    </Fragment>
+  );
+}
+
+export default App;
